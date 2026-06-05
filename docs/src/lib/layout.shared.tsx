c67daf0088792a0b95bbe997fc3fc56827c7c2ce
Nath <167110605+RwNath@users.noreply.github.com>
2026-06-05 18:28:56 +0200

    chore: great improvement of the docs

--- docs/src/lib/layout.shared.tsx
+++ docs/src/lib/layout.shared.tsx
@@ -2,6 +2,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
+    githubUrl: "https://github.com/axtraz/transpic",
     nav: {
       title: 'Transpic Docs',
       transparentMode: "always",
