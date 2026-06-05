733450210842c8561e1b4c0b941f89eec5674e24
Nath <167110605+RwNath@users.noreply.github.com>
2026-06-05 19:05:00 +0200

    feat: add opengraph

--- docs/src/lib/layout.shared.tsx
+++ docs/src/lib/layout.shared.tsx
@@ -1,10 +1,12 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
+export const appName = "Transpic Docs";
+
 export function baseOptions(): BaseLayoutProps {
   return {
     githubUrl: "https://github.com/axtraz/transpic",
     nav: {
-      title: 'Transpic Docs',
+      title: appName,
       transparentMode: "always",
     },
   };
