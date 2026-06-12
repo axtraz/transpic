938b4c4bb28617bc98017538d4f77666591b7f14
Nath <167110605+RwNath@users.noreply.github.com>
2026-06-12 17:13:31 +0200

    chore: format

--- core/src/huerotate.rs
+++ core/src/huerotate.rs
@@ -2,6 +2,6 @@ use image::DynamicImage;
 
 pub fn huerotate(img: DynamicImage, degrees: i32) -> Result<DynamicImage, String> {
   let rotated = image::imageops::huerotate(&img, degrees);
-  
+
   Ok(DynamicImage::ImageRgba8(rotated))
 }
